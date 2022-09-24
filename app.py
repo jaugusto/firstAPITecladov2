8eebaba9b97f8bcd5e004c5b97840e4091b8183f
jaugusto <dev@augustolustosa.com>
2022-09-24 16:30:29 -0300

    add update and delete endpoints

--- app.py
+++ app.py
@@ -6,17 +6,9 @@ from flask_smorest import abort
 app = Flask(__name__)
 
 
-@app.get('/store')
-def index():
-    return {"stores": list(stores.values())}, 202
-
-
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
-    try:
-        return stores[store_id], 202
-    except KeyError:
-        abort(404, message="store not found.")
+@app.get('/item')
+def get_items():
+    return {"items": list(items.values())}, 202
 
 
 @app.get('/item/<string:item_id>')
@@ -25,25 +17,6 @@ def get_item(item_id):
         return items[item_id], 202
     except KeyError:
         abort(404, message="item not found.")
-
-
-@app.get('/item')
-def get_items():
-    return {"items": list(items.values())}, 202
-
-
-@app.post('/store')
-def create_store():
-    data_store = request.get_json()
-    if 'name' not in data_store:
-        abort(400, message="Bad request. Ensure 'name' is included in JSON payload")
-    for store in stores.values():
-        if data_store['name'] == store['name']:
-            abort(400, message="store already created")
-    store_id = uuid.uuid4().hex
-    new_store = {"id": store_id, **data_store}
-    stores[store_id] = new_store
-    return new_store, 201
 
 
 @app.post('/item')
@@ -60,6 +33,64 @@ def create_item():
     new_item = {"id": item_id, **data_item}
     items[item_id] = new_item
     return new_item
+
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        abort(404, message="Bad request. Ensure 'name' or 'price' in JSON payload")
+    try:
+        item = items[item_id]
+        item = {**item, **item_data}
+        return item
+    except KeyError:
+        abort(404, message="item not found")
+
+
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "item deleted"}
+    except KeyError:
+        abort(404, message="item not found")
+
+
+@app.get('/store')
+def index():
+    return {"stores": list(stores.values())}, 202
+
+
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id], 202
+    except KeyError:
+        abort(404, message="store not found.")
+
+
+@app.post('/store')
+def create_store():
+    data_store = request.get_json()
+    if 'name' not in data_store:
+        abort(400, message="Bad request. Ensure 'name' is included in JSON payload")
+    for store in stores.values():
+        if data_store['name'] == store['name']:
+            abort(400, message="store already created")
+    store_id = uuid.uuid4().hex
+    new_store = {"id": store_id, **data_store}
+    stores[store_id] = new_store
+    return new_store, 201
+
+
+@app.delete('/store/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "store deleted."}
+    except KeyError:
+        abort(404, message="store not found")
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,16 +1,3 @@
 stores = {}
 
-items = {
-    1:  {
-        "name": "uva",
-        "price": 3
-    },
-    2: {
-        "name": "maca",
-        "price": 4
-    },
-    3: {
-        "name": "melancia",
-        "price": 12
-    }
-}
+items = {}
